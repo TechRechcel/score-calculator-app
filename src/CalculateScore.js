28adfa837ad0312f0a250880bd6de925ca09154f
rechcel toledo <rechcel@rechcel-R460.lan>
2022-11-03 16:08:41 +0800

    first commit

---
+++ src/CalculateScore.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+import React from 'react';
+
+
+
+export default function CalculateScore() {
+  const [color, setColor] = useState("red");
+    
+  return (
+    <table ClassName="wrapper">
+      <input type="text" />
+      <input type="number" />
+      <input type="number" />
+    </table>
+  );
+}
